50164a6cb75fedbd1bd9b9654ce46a93d10e1af3
Abdul Ahmad <abdul.as.ahmad@gmail.com>
2021-09-02 14:41:33 +1000

    Implement basic routing and add some global styles

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Landing from "./pages/Landing/Landing";
+import SignUp from "./pages/SignUp/SignUp";
+import SignIn from "./pages/SignIn/SignIn";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
-    <div >
-          The basic empty template
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/home" component={Home} />
+        <Route path="/" component={Landing} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
