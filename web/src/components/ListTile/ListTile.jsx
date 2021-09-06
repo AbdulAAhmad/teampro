030343673883f49751d82ad74ea86ff4c0cc6cbc
Abdul Ahmad <abdul.as.ahmad@gmail.com>
2021-09-07 02:19:59 +1000

    Implement socket.io messaging and add user status indicator

--- web/src/components/ListTile/ListTile.jsx
+++ web/src/components/ListTile/ListTile.jsx
@@ -3,11 +3,11 @@ import "./ListTile.css";
 
 import { FaUserCircle } from "react-icons/fa";
 
-const ListTile = ({ name, children }) => {
+const ListTile = ({ name, children, online = false }) => {
   return (
     <div className="list-tile">
       <div className="leading">
-        <FaUserCircle className="icon" />
+        <FaUserCircle className="icon" color={online ? "green" : "#e8e8e8"} />
         <h3>{name}</h3>
       </div>
       <div className="trailing">{children}</div>
--- web/src/components/MessageCard/MessageCard.jsx
+++ web/src/components/MessageCard/MessageCard.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./MessageCard.css";
+
+import io from "socket.io-client";
 
 import CustomButton from "../CustomButton/CustomButton";
 import ListTile from "../ListTile/ListTile";
@@ -9,7 +11,7 @@ import useDataApi from "../../hooks/useDataApi";
 const USERS_ROOT_URL = `${process.env.REACT_APP_API_URL}/users`;
 
 const MessageCard = () => {
-  const { token } = useAuthState();
+  const { token, user_id } = useAuthState();
 
   const [{ data: users }] = useDataApi(USERS_ROOT_URL, [], {
     method: "GET",
@@ -17,6 +19,48 @@ const MessageCard = () => {
   });
 
   const [recipients, setRecipients] = useState([]);
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+  const [connectedUsers, setConnectedUsers] = useState([]);
+
+  const [socket, setSocket] = useState();
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:5000", { autoConnect: false });
+
+    newSocket.auth = { user_id };
+    newSocket.connect();
+
+    setSocket(newSocket);
+
+    return () => newSocket.close();
+  }, [user_id]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("message", (res) => {
+        setResponse(res);
+        // console.log(res);
+      });
+
+      socket.on("users", (res) => {
+        setConnectedUsers(res.map((user) => user.user_id));
+        console.log(res);
+      });
+    }
+    return () => {
+      if (socket) {
+        socket.off("message");
+        socket.off("users");
+      }
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    if (response) {
+      console.log(response);
+    }
+  }, [response]);
 
   const handleCheckbox = (e) =>
     e.target.checked
@@ -26,16 +70,28 @@ const MessageCard = () => {
   const handleSelectAll = () =>
     setRecipients(() => users.map((user) => user.id));
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (socket && socket.connected) {
+      socket.emit("message", { message, recipients, sender: user_id });
+    }
+    setMessage("");
+  };
+
   return (
     <div className="message-card">
       <div className="message-card-header">
         <h2>The Team</h2>
         <CustomButton onClick={handleSelectAll}>Select All</CustomButton>
       </div>
-      <form action="" className="message-card-form">
+      <form action="" onSubmit={handleSubmit} className="message-card-form">
         {users &&
           users.map((user) => (
-            <ListTile key={user.id} name={user.name}>
+            <ListTile
+              key={user.id}
+              name={user.name}
+              online={connectedUsers.includes(user.id)}
+            >
               <input
                 type="checkbox"
                 name={user.id}
@@ -44,8 +100,11 @@ const MessageCard = () => {
               />
             </ListTile>
           ))}
-        <textarea />
-        <CustomButton>Send Message</CustomButton>
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <CustomButton type="submit">Send Message</CustomButton>
       </form>
     </div>
   );
